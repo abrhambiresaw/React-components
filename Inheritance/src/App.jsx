02fcc7b3c1dca4dd1517a8fd61d69cddd90f335c
Abrham Biresaw <abrhambiresaw2112@gmail.com>
2026-05-22 14:07:31 +0300

    change the name

--- Inheritance/src/App.jsx
+++ Inheritance/src/App.jsx
@@ -3,7 +3,7 @@ import Student from "./Student";
 function App() {
   return (
     <div>
-      <Student name="Abrham" age={22} />
+      <Student name="Abel" age={22} />
       <Student name="Sara" age={20} />
     </div>
   );
