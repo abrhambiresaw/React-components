6cb5e308ab185a87c44ff315bdec48b9e4afd2f6
Abrham Biresaw <abrhambiresaw2112@gmail.com>
2026-05-22 13:55:49 +0300

    create student.jsx file and create props

---
+++ Inheritance/src/App.jsx
@@ -0,0 +1,12 @@
+import Student from "./Student";
+
+function App() {
+  return (
+    <div>
+      <Student name="Abrham" age={22} />
+      <Student name="Sara" age={20} />
+    </div>
+  );
+}
+
+export default App;
