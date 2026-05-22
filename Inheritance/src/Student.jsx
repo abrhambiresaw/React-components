620d455036da8a9bcdc430b849544f9a83399a98
Abrham Biresaw <abrhambiresaw2112@gmail.com>
2026-05-22 13:59:50 +0300

    destructuring props

--- Inheritance/src/Student.jsx
+++ Inheritance/src/Student.jsx
@@ -1,8 +1,8 @@
 function Student(props) {
   return (
     <div>
-      <h1>Name: {props.name}</h1>
-      <p>Age: {props.age}</p>
+      <h1>Name: {name}</h1>
+      <p>Age: {age}</p>
     </div>
   );
 }
